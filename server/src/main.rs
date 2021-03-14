64b1ebaefd927b17caef42c5597866bf3056e9db
Tahar <tntmeijs@gmail.com>
2021-03-14 15:48:06 +0100

    404 page

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,14 +1,16 @@
-use actix_files as fs;
-use actix_web::{App, HttpServer};
+use actix_files::{NamedFile};
+use actix_web::{web, App, HttpServer, HttpResponse, HttpRequest, Result};
+
+async fn not_found(req: HttpRequest) -> Result<HttpResponse> {
+    NamedFile::open("./static/not_found.html")?.into_response(&req)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        App::new().service(
-            fs::Files::new("/", "./static")
-                .show_files_listing()
-                .use_last_modified(true),
-        )
+        App::new()
+            .service(actix_files::Files::new("/", "./static").index_file("index.html")
+        ).default_service(web::to(not_found))
     })
     .bind("127.0.0.1:8080")?
     .run()
