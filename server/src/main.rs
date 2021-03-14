1ef78c0a08754f285b6756747b8deb679a8341e2
Tahar <tntmeijs@gmail.com>
2021-03-14 16:09:17 +0100

    Host a React.js application

--- server/src/main.rs
+++ server/src/main.rs
@@ -2,14 +2,15 @@ use actix_files::{NamedFile};
 use actix_web::{web, App, HttpServer, HttpResponse, HttpRequest, Result};
 
 async fn not_found(req: HttpRequest) -> Result<HttpResponse> {
-    NamedFile::open("./static/not_found.html")?.into_response(&req)
+    NamedFile::open("./public/index.html")?.into_response(&req)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(actix_files::Files::new("/", "./static").index_file("index.html")
+            .service(actix_files::Files::new("/", "./public")
+            .index_file("index.html")
         ).default_service(web::to(not_found))
     })
     .bind("127.0.0.1:8080")?
