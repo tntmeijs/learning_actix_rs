88dcfaa461ccce5ab8b8abbd384f39fe4101b644
Tahar <tntmeijs@gmail.com>
2021-03-14 16:26:22 +0100

    Very simple logic to fetch data from the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+import RenderIf from "./RenderIf";
+
 import './App.css';
 
 export default function App() {
+    const [data, setData] = useState([]);
+
+    const onFetchData = () => {
+        fetch("/api/all")
+            .then(response => {
+                console.log(response.status);
+            })
+            .catch(error => console.error(error));
+    };
+
     return (
-        <h1>Hello, React.js!</h1>
+        <>
+            <h1>Learning Actix.rs</h1>
+            <input type="button" value="fetch data" onClick={onFetchData} />
+
+            <RenderIf condition={data.length === 0}>
+                <h3>No data available</h3>
+            </RenderIf>
+
+            <RenderIf condition={data.length > 0}>
+                <li>
+                    {data.map((entry, index) => {
+                        return (
+                            <ul key={index}>{entry}</ul>
+                        );
+                    })}
+                </li>
+            </RenderIf>
+        </>
     );
 }
